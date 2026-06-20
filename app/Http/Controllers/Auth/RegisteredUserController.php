3f8ea23a8fd3a7b3fb39d1088aa2c83b6d9632fc
tdi <tdi@tesda.gov.ph>
2026-06-20 17:38:33 +0800

    Fix: redirect new users to home page instead of dashboard after registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -126,7 +126,7 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
         Auth::login($user);
 
-        return to_route('dashboard');
+        return redirect('/');
     }
 
     public function otpPage(Request $request): Response
